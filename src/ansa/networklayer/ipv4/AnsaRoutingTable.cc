03cc7494ceb36daaea5aa9aae2811c16fc13819e
screw <xscrew02@gmail.com>
2013-12-02 19:46:12 +0100

    Commented out double deallocation of routes.

--- src/ansa/networklayer/ipv4/AnsaRoutingTable.cc
+++ src/ansa/networklayer/ipv4/AnsaRoutingTable.cc
@@ -44,10 +44,10 @@ std::ostream& operator<<(std::ostream& os, const AnsaIPv4MulticastRoute& e)
  */
 AnsaRoutingTable::~AnsaRoutingTable()
 {
-    for (unsigned int i=0; i<routes.size(); i++)
-        delete routes[i];
-    for (unsigned int i=0; i<multicastRoutes.size(); i++)
-        delete multicastRoutes[i];
+//    for (unsigned int i=0; i<routes.size(); i++)
+//        delete routes[i];
+//    for (unsigned int i=0; i<multicastRoutes.size(); i++)
+//        delete multicastRoutes[i];
 }
 
 IPv4Route *AnsaRoutingTable::findRoute(const IPv4Address& network, const IPv4Address& netmask)
