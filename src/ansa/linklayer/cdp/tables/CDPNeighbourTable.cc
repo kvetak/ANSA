6d881d34420011b5dbde57a560e0801d77653326
rajcatomas <rajcatomas@gmail.com>
2016-04-21 09:19:43 +0200

    move files

---
+++ src/ansa/linklayer/cdp/tables/CDPNeighbourTable.cc
@@ -0,0 +1,167 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "ansa/linklayer/cdp/CDPNeighbourTable.h"
+
+namespace inet {
+
+CDPNeighbour::CDPNeighbour():
+        interface(nullptr), address(nullptr), odrDefaultGateway(nullptr)
+{
+    holdtimeTimer = new CDPTimer();
+    holdtimeTimer->setTimerType(Holdtime);
+    holdtimeTimer->setContextPointer(this);
+}
+
+CDPNeighbour::~CDPNeighbour() {
+    if(holdtimeTimer != nullptr)
+    {
+        //if is scheduled, get his sender module, otherwise get owner module
+        cSimpleModule *owner = dynamic_cast<cSimpleModule *>((holdtimeTimer->isScheduled()) ? holdtimeTimer->getSenderModule() : holdtimeTimer->getOwner());
+        if(owner != NULL)
+        {// owner is cSimpleModule object -> can call his methods
+            owner->cancelAndDelete(holdtimeTimer);
+            holdtimeTimer = NULL;
+        }
+    }
+}
+
+std::string CDPNeighbour::info() const
+{
+    std::stringstream out;
+
+    out << name << ", local int: " << interface->getName();
+    out << ", holdtime: " << round(ttl.dbl()-(simTime()-lastUpdate).dbl()) << ", cap: " << capabilities;
+    out << ", send int: " << portSend;
+    return out.str();
+}
+
+/**
+ * Get cdp neighbour from neighbour table by name and receive port
+ *
+ * @param   name    name of neighbour
+ * @param   port    receive port number
+ *
+ * @return  cdp neighbour
+ */
+CDPNeighbour * CDPNeighbourTable::findNeighbour(std::string name, int port)
+{
+    std::vector<CDPNeighbour *>::iterator it;
+
+    for (it = neighbours.begin(); it != neighbours.end(); ++it)
+    {// through all neighbours search for same neighbour with same name and port
+        if((*it)->getName() == name && (*it)->getPortReceive() == port)
+        {// found same
+            return (*it);
+        }
+    }
+
+    return nullptr;
+}
+
+/**
+ * add neighbour to neighbour table
+ *
+ * @param   neighbour   neighbour to add
+ */
+void CDPNeighbourTable::addNeighbour(CDPNeighbour * neighbour)
+{
+    if(findNeighbour(neighbour->getName(), neighbour->getPortReceive()) != NULL)
+    {// neighbour already in table
+        //TODO: otestovat %s
+        throw cRuntimeError("Adding to CDPNeighbourTable neighbour, which is already in it - name %s, port %d", neighbour->getName(), neighbour->getPortReceive());
+    }
+
+    neighbours.push_back(neighbour);
+}
+
+
+/**
+ * Remove all neighbours
+ *
+ */
+void CDPNeighbourTable::removeNeighbours()
+{
+    std::vector<CDPNeighbour *>::iterator it;
+
+    for (it = neighbours.begin(); it != neighbours.end();)
+    {
+        delete (*it);
+        it = neighbours.erase(it);
+    }
+}
+
+/**
+ * Remove neighbour
+ *
+ * @param   neighbour   neighbour to delete
+ *
+ */
+void CDPNeighbourTable::removeNeighbour(CDPNeighbour * neighbour)
+{
+    std::vector<CDPNeighbour *>::iterator it;
+
+    for (it = neighbours.begin(); it != neighbours.end();)
+    {// through all neighbours
+        if((*it) == neighbour)
+        {// found same
+            delete (*it);
+            it = neighbours.erase(it);
+            return;
+        }
+        else
+        {// do not delete -> get next
+            ++it;
+        }
+    }
+}
+
+/**
+ * Removes neighbours
+ *
+ * @param   name    name of neighbour
+ * @param   port    receive port number
+ */
+void CDPNeighbourTable::removeNeighbour(std::string name, int port)
+{
+    std::vector<CDPNeighbour *>::iterator it;
+
+    for (it = neighbours.begin(); it != neighbours.end();)
+    {// through all neighbours search for same neighbour with same name and port
+        if((*it)->getName() == name && (*it)->getPortReceive() == port)
+        {// found same
+            delete (*it);
+            it = neighbours.erase(it);
+            return;
+        }
+        else
+        {// do not delete -> get next
+            ++it;
+        }
+    }
+}
+
+CDPNeighbourTable::~CDPNeighbourTable()
+{
+    std::vector<CDPNeighbour *>::iterator it;
+
+    for (it = neighbours.begin(); it != neighbours.end(); ++it)
+    {// through all neighbours
+        delete (*it);
+    }
+    neighbours.clear();
+}
+
+} /* namespace inet */
---
+++ src/ansa/linklayer/cdp/tables/CDPODRRouteTable.h
@@ -0,0 +1,99 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef CDPODRROUTETABLE_H_
+#define CDPODRROUTETABLE_H_
+
+#include "ansa/linklayer/cdp/CDPTimer_m.h"
+#include "inet/networklayer/contract/IRoute.h"
+#include "inet/networklayer/ipv4/IPv4RoutingTable.h"
+
+
+namespace inet {
+
+
+class CDPODRRoute : public cObject
+{
+    private:
+      IRoute *route = nullptr;    // the route in the host routing table that is associated with this route, may be nullptr if deleted
+      L3Address dest;    // destination of the route
+      int prefixLength = 0;    // prefix length of the destination
+      L3Address nextHop;    // next hop of the route
+      InterfaceEntry *ie = nullptr;    // outgoing interface of the route
+      bool invalide = false;    // true if the route has changed since the update
+      bool noUpdates = false;    // true if the route has changed since the update
+      simtime_t lastUpdateTime;    // time of the last change, only for RTE routes
+      CDPTimer *ODRInvalideTime;
+      CDPTimer *ODRHolddown;
+      CDPTimer *ODRFlush;
+
+      void createTimers();
+
+    public:
+      CDPODRRoute(IRoute *route);
+      CDPODRRoute(L3Address des, int pre, L3Address nex, InterfaceEntry *i);
+      virtual ~CDPODRRoute();
+      virtual std::string info() const override;
+
+      void deleteTimer(CDPTimer *timer);
+      void removeODRRoutes();
+
+      // getters
+      IRoute *getRoute() const { return route; }
+      L3Address getDestination() const { return dest; }
+      int getPrefixLength() const { return prefixLength; }
+      L3Address getNextHop() const { return nextHop; }
+      InterfaceEntry *getInterface() const { return ie; }
+      bool isInvalide() const { return invalide; }
+      bool isNoUpdates() const { return noUpdates; }
+      simtime_t getLastUpdateTime() const { return lastUpdateTime; }
+      CDPTimer *getODRInvalideTime() const { return ODRInvalideTime; }
+      CDPTimer *getODRHolddown() const { return ODRHolddown; }
+      CDPTimer *getODRFlush() const { return ODRFlush; }
+
+      // setters
+      void setRoute(IRoute *route) { this->route = route; }
+      void setDestination(const L3Address& dest) { this->dest = dest; }
+      void setPrefixLength(int prefixLength) { this->prefixLength = prefixLength; }
+      void setNextHop(const L3Address& nextHop) { this->nextHop = nextHop; route->setNextHop(nextHop); }
+      void setInterface(InterfaceEntry *ie) { this->ie = ie; route->setInterface(ie); }
+      void setInvalide(bool invalide) { this->invalide = invalide; }
+      void setNoUpdates(bool noUpdates) { this->noUpdates = noUpdates; }
+      void setLastUpdateTime(simtime_t time) { lastUpdateTime = time; }
+      void setODRInvalideTime(CDPTimer *time) { ODRInvalideTime = time; }
+      void setODRHolddown(CDPTimer *time) { ODRHolddown = time; }
+      void setODRFlush(CDPTimer *time) { ODRFlush = time; }
+};
+
+class CDPODRRouteTable {
+protected:
+  std::vector<CDPODRRoute *> routes;
+
+public:
+  virtual ~CDPODRRouteTable();
+
+  std::vector<CDPODRRoute *>& getRoutes() {return routes;}
+  CDPODRRoute *findRoute(const L3Address& destination, int prefixLength, const L3Address& nextHope);
+  CDPODRRoute *findRoute(const IRoute *route);
+  CDPODRRoute *existOtherDefaultRoute(CDPODRRoute *route);
+  int countDestinationPaths(const L3Address& destination, int prefixLength);
+  void addRoute(CDPODRRoute * route);
+  void removeRoutes();
+  void removeRoute(CDPODRRoute * route);
+};
+
+} /* namespace inet */
+
+#endif /* CDPODRROUTETABLE_H_ */
