80355a0065302f2e37c52bd96aaccd595f4b3d27
Jan Holusa <xholus02@stud.fit.vutbr.cz>
2016-05-07 20:39:56 +0200

    glbp comments added

--- src/ansa/networklayer/glbp/GLBPMessage.h
+++ src/ansa/networklayer/glbp/GLBPMessage.h
@@ -17,12 +17,15 @@
 #define GLBPMESSAGE_H_
 
 #include "inet/common/INETDefs.h"
-//#include "inet/networklayer/contract/INetworkDatagram.h"
 #include "ansa/networklayer/glbp/GLBPMessage_m.h"
 
 namespace inet {
-
-class GLBPMessage : public GLBPMessage_Base//, public INetworkDatagram
+/**
+* @file GLBPMessage.h
+* @author Jan Holusa
+* @brief Extended message file, needed because of TLV
+*/
+class GLBPMessage : public GLBPMessage_Base
 {
     private:
       void copy(const GLBPMessage& other);
@@ -32,34 +35,7 @@ class GLBPMessage : public GLBPMessage_Base//, public INetworkDatagram
       GLBPMessage(const char *name = nullptr, int kind = 0) : GLBPMessage_Base(name, kind) {}
       GLBPMessage(const GLBPMessage& other) : GLBPMessage_Base(other) {}
       GLBPMessage& operator=(const GLBPMessage& other) { GLBPMessage_Base::operator=(other); return *this; }
-
       virtual GLBPMessage *dup() const override { return new GLBPMessage(*this); }
-
-      /**
-       * getter/setter for totalLength field in datagram
-       * if set to -1, then getter returns getByteLength()
-       */
-//      int getTotalLengthField() const override;
-
-//      /**
-//       * Returns bits 0-5 of the Type of Service field, a value in the 0..63 range
-//       */
-//      virtual int getDiffServCodePoint() const override { return getTypeOfService() & 0x3f; }
-
-//      /**
-//       * Sets bits 0-5 of the Type of Service field; expects a value in the 0..63 range
-//       */
-//      virtual void setDiffServCodePoint(int dscp) override { setTypeOfService((getTypeOfService() & 0xc0) | (dscp & 0x3f)); }
-
-//      /**
-//       * Returns bits 6-7 of the Type of Service field, a value in the range 0..3
-//       */
-//      virtual int getExplicitCongestionNotification() const override { return (getTypeOfService() >> 6) & 0x03; }
-
-//      /**
-//       * Sets bits 6-7 of the Type of Service; expects a value in the 0..3 range
-//       */
-//      virtual void setExplicitCongestionNotification(int ecn) override { setTypeOfService((getTypeOfService() & 0x3f) | ((ecn & 0x3) << 6)); }
 
       /**
        * Returns the number of extension headers in this datagram
@@ -84,21 +60,6 @@ class GLBPMessage : public GLBPMessage_Base//, public INetworkDatagram
        * default atPos means add to the end.
        */
       virtual void addOption(TLVOptionBase *opt, int atPos = -1);
-
-//      /**
-//       * Calculates the length of the IPv6 header plus the extension
-//       * headers.
-//       */
-//      virtual int calculateHeaderByteLength() const;
-
-
-//      virtual L3Address getSourceAddress() const override { return L3Address(getSrcAddress()); }
-//      virtual void setSourceAddress(const L3Address& address) override { setSrcAddress(address.toIPv4()); }
-//      virtual L3Address getDestinationAddress() const override { return L3Address(getDestAddress()); }
-//      virtual void setDestinationAddress(const L3Address& address) override { setDestAddress(address.toIPv4()); }
-//      virtual int getTransportProtocol() const override { return GLBPMessage_Base::getTransportProtocol(); }
-//      virtual void setTransportProtocol(int protocol) override { GLBPMessage_Base::setTransportProtocol(protocol); }
-
 };
 
 } /* namespace inet */
